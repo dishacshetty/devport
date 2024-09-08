374bd58654aa86c565b4fdacaab93bf1a11b36b9
dishacshetty <138439813+dishacshetty@users.noreply.github.com>
2024-09-08 23:24:25 +0530

    Update settings.py

--- devport/settings.py
+++ devport/settings.py
@@ -26,7 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.devport.com']
+
+CSRF_TRUSTED_ORIGINS = [
+    'www.devport.com'
+]
+
 
 
 
